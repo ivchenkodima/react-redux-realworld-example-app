0fc03012bac270967746749995d99b0ac5bf8238
ivchenkodima <ivchenkodima@gmail.com>
2017-11-09 20:39:14 +0200

    remove

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { ConnectedRouter } from 'react-router-redux';
 
 import App from './components/App';
 
-ReactDOM.render((
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>
-
-), document.getElementById('root'));
+// ReactDOM.render((
+//   <Provider store={store}>
+//     <ConnectedRouter history={history}>
+//       <Switch>
+//         <Route path="/" component={App} />
+//       </Switch>
+//     </ConnectedRouter>
+//   </Provider>
+//
+// ), document.getElementById('root'));
