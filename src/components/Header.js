905d28264d8fe881c728603355ec9a2f098f0c47
amatiyko <Andrew.matiyko@gmail.com>
2017-11-09 20:08:45 +0200

    hello world (#9)

--- src/components/Header.js
+++ src/components/Header.js
@@ -103,6 +103,7 @@ class Header extends React.Component {
     return (
       <nav className="navbar navbar-light">
         <div className="container">
+            <p>HELLO WORLD</p>
 
             <button onClick={()=>alert('ho-ho-hodor')}>Ho-Ho-Hodor</button>
           <Link to="/" className="navbar-brand">
